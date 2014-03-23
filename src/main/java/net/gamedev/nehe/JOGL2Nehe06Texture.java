e2b6501a2ba261d90854c683b7685d1ef647aa19
Samael Bate <singingbush@hotmail.com>
2014-03-23 15:26:16 +0000

    added two launchers for nehe6 demo. Now can see how to open using either a Swing window or NEWT

--- src/main/java/net/gamedev/nehe/JOGL2Nehe06Texture.java
+++ src/main/java/net/gamedev/nehe/JOGL2Nehe06Texture.java
@@ -1,19 +1,11 @@
 package net.gamedev.nehe;
 
-import java.awt.Dimension;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLException;
-import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.glu.GLU;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
-import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.TextureCoords;
 import com.jogamp.opengl.util.texture.TextureIO;
@@ -26,51 +18,7 @@ import static javax.media.opengl.GL2.*; // GL2 constants
  * @version May 2012
  */
 @SuppressWarnings("serial")
-public class JOGL2Nehe06Texture extends GLCanvas implements GLEventListener {
-
-    // Define constants for the top-level container
-    private static String TITLE = "NeHe Lesson #6: Texture - with JOGL2 + Swing";
-    private static final int CANVAS_WIDTH = 640;  // width of the drawable
-    private static final int CANVAS_HEIGHT = 480; // height of the drawable
-    private static final int FPS = 60; // animator's target frames per second
-
-    /** The entry main() method to setup the top-level container and animator */
-    public static void main(String[] args) {
-        // Run the GUI codes in the event-dispatching thread for thread safety
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                // Create the OpenGL rendering canvas
-                GLCanvas canvas = new JOGL2Nehe06Texture();
-                canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
-
-                // Create a animator that drives canvas' display() at the specified FPS.
-                final FPSAnimator animator = new FPSAnimator(canvas, FPS, true);
-
-                // Create the top-level container
-                final JFrame frame = new JFrame(); // Swing's JFrame or AWT's Frame
-                frame.getContentPane().add(canvas);
-                frame.addWindowListener(new WindowAdapter() {
-                    @Override
-                    public void windowClosing(WindowEvent e) {
-                        // Use a dedicate thread to run the stop() to ensure that the
-                        // animator stops before program exits.
-                        new Thread() {
-                            @Override
-                            public void run() {
-                                if (animator.isStarted()) animator.stop();
-                                System.exit(0);
-                            }
-                        }.start();
-                    }
-                });
-                frame.setTitle(TITLE);
-                frame.pack();
-                frame.setVisible(true);
-                animator.start(); // start the animation loop
-            }
-        });
-    }
+public class JOGL2Nehe06Texture implements GLEventListener {
 
     // Setup OpenGL Graphics Renderer
 
@@ -93,10 +41,6 @@ public class JOGL2Nehe06Texture extends GLCanvas implements GLEventListener {
     // top, bottom, left and right coordinates.
     private float textureTop, textureBottom, textureLeft, textureRight;
 
-    /** Constructor to setup the GUI for this Component */
-    public JOGL2Nehe06Texture() {
-        this.addGLEventListener(this);
-    }
 
     // ------ Implement methods declared in GLEventListener ------
 
---
+++ src/main/java/net/gamedev/nehe/JOGL2Nehe06TextureNewtWindow.java
@@ -0,0 +1,55 @@
+package net.gamedev.nehe;
+
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.util.FPSAnimator;
+
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLProfile;
+
+/**
+ * Create a window using NEWT
+ */
+public class JOGL2Nehe06TextureNewtWindow {
+
+    private static final String TITLE = "JOGL2 Example using NEWT";
+    private static final int WINDOW_WIDTH = 640;  // width of the drawable
+    private static final int WINDOW_HEIGHT = 480; // height of the drawable
+    private static final int FPS = 60; // animator's target frames per second
+
+    public static void main(String[] args) {
+        // Get the default OpenGL profile, reflecting the best for your running platform
+        GLProfile glp = GLProfile.getDefault();
+
+        // Specifies a set of OpenGL capabilities, based on your profile.
+        GLCapabilities caps = new GLCapabilities(glp);
+        // Create the OpenGL rendering canvas
+        GLWindow window = GLWindow.create(caps);
+
+        // Create a animator that drives canvas' display() at the specified FPS.
+        final FPSAnimator animator = new FPSAnimator(window, FPS, true);
+
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDestroyNotify(WindowEvent arg0) {
+                // Use a dedicate thread to run the stop() to ensure that the
+                // animator stops before program exits.
+                new Thread() {
+                    @Override
+                    public void run() {
+                        animator.stop(); // stop the animator loop
+                        System.exit(0);
+                    }
+                }.start();
+            }
+        });
+
+        window.addGLEventListener(new JOGL2Nehe06Texture());
+
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setTitle(TITLE);
+        window.setVisible(true);
+        animator.start();
+    }
+}
